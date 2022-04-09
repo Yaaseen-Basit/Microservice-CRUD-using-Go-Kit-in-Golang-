3bb9086b5d407a74d69e1579038e92e01561ba50
Yaaseen-Basit <44226750+Yaaseen-Basit@users.noreply.github.com>
2022-04-09 17:18:32 +0530

    Microservices CRUD

---
+++ dbconnect.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "gopkg.in/go-sql-driver/mysql.v1"
+)
+
+var db *sql.DB
+
+// GetMongoDB function to return DB connection
+func GetDBconn() *sql.DB {
+	dbName := "go_ms_test"
+	fmt.Println("conn info:", dbName)
+	db, err := sql.Open("mysql",
+		"root:root@tcp(127.0.0.1:3306)/go_ms_test")
+	if err != nil {
+		panic(err.Error())
+		
+	}
+	fmt.Println("err info:", err)
+	fmt.Println("db info:", err)
+
+
+	// defer db.Close()
+
+	return db
+}
